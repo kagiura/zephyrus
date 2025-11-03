ff20b0f0d1927fbd113b05c09e5f328b8ded3e5b
kagiura <58155530+kagiura@users.noreply.github.com>
2025-11-03 17:26:38 +0800

    feat(skylark): ✨ animations :sparkles:

--- skylark/src/app/(nav)/page.tsx
+++ skylark/src/app/(nav)/page.tsx
@@ -1,32 +1,93 @@
 "use client";
-import { Box, Flex, TextField } from "@radix-ui/themes";
+import {
+	Box,
+	Flex,
+	IconButton,
+	Inset,
+	Text,
+	TextField,
+} from "@radix-ui/themes";
 
 import CampusSearchResults from "@/components/CampusSearchResults";
 import ClosestBuilding from "@/components/ClosestBuilding";
 import ClosestStops from "@/components/ClosestStops";
-import { LayoutGroup } from "motion/react";
+import { IconCross, IconSearch, IconX } from "@tabler/icons-react";
+import { AnimatePresence, LayoutGroup, motion } from "motion/react";
 import { useState } from "react";
 
 export default function Page() {
 	const [searchQuery, setSearchQuery] = useState("");
+	const [isSearching, setIsSearching] = useState(false);
 
 	return (
 		<>
-			<TextField.Root
-				placeholder="Search NUS"
-				value={searchQuery}
-				onChange={(e) => setSearchQuery(e.target.value)}
-			/>
-			{searchQuery ? (
-				<CampusSearchResults searchQuery={searchQuery} />
-			) : (
-				<Flex direction="column" gap="0" mt="4">
-					<LayoutGroup>
-						<ClosestBuilding />
-						<ClosestStops />
-					</LayoutGroup>
-				</Flex>
-			)}
+			<Box mb="5">
+				<TextField.Root
+					placeholder="Search NUS"
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+					onFocus={() => setIsSearching(true)}
+					onBlur={() => {
+						// searching false if query is empty
+						if (searchQuery.trim() === "") {
+							setIsSearching(false);
+						}
+					}}
+					color="gray"
+					variant="soft"
+					radius="large"
+				>
+					<TextField.Slot side="left">
+						<Text color="gray">
+							<IconSearch width={12} height={12} />
+						</Text>
+					</TextField.Slot>
+					<TextField.Slot side="right">
+						{!!searchQuery && (
+							<IconButton
+								color="gray"
+								variant="ghost"
+								size="1"
+								onClick={() => {
+									setSearchQuery("");
+									setIsSearching(false);
+								}}
+							>
+								<IconX width={12} height={12} />
+							</IconButton>
+						)}
+					</TextField.Slot>
+				</TextField.Root>
+			</Box>
+
+			<AnimatePresence>
+				{isSearching ? (
+					<motion.div
+						key="search-results"
+						initial={{ opacity: 0, x: 10 }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0, x: 10, position: "absolute" }}
+						transition={{ damping: 120 }}
+					>
+						<CampusSearchResults searchQuery={searchQuery} />
+					</motion.div>
+				) : (
+					<motion.div
+						key="closest-info"
+						initial={{ opacity: 0, x: -10 }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0, x: -10, position: "absolute" }}
+						transition={{ damping: 120 }}
+					>
+						<Flex direction="column" gap="0" mt="4">
+							<LayoutGroup>
+								<ClosestBuilding />
+								<ClosestStops />
+							</LayoutGroup>
+						</Flex>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</>
 	);
 }
--- skylark/src/components/ClosestBuilding.tsx
+++ skylark/src/components/ClosestBuilding.tsx
@@ -2,7 +2,13 @@ import { IMDF_BUILDINGS, IMDF_LEVELS } from "@/data/imdf";
 import { useMapState } from "@/utils/mapState";
 import { useDodgeUI } from "@/utils/useDodgeUI";
 import { Building, Level } from "@dazzlegarden/types/imdf";
-import { Card, Flex, Text } from "@radix-ui/themes";
+import { Box, Card, Flex, Grid, IconButton, Text } from "@radix-ui/themes";
+import {
+	IconBowlChopsticks,
+	IconBus,
+	IconDroplet,
+	IconToiletPaper,
+} from "@tabler/icons-react";
 import { point } from "@turf/helpers";
 import pointToPolygonDistance from "@turf/point-to-polygon-distance";
 import { AnimatePresence, motion } from "motion/react";
@@ -10,6 +16,8 @@ import { useMemo, useState } from "react";
 
 const CLOSE_UP_ZOOM_THRESHOLD = 18;
 const DISTANCE_THRESHOLD_METERS = 70;
+
+// reminder to self this is closest to map centerpoint not user location
 function ClosestBuilding() {
 	const { levelOrdinal, debouncedZoom } = useMapState();
 	const { correctedCenter } = useDodgeUI();
@@ -54,24 +62,109 @@ function ClosestBuilding() {
 					animate={{ opacity: 1, height: "auto" }}
 					exit={{ opacity: 0, height: 0 }}
 					layout
-					style={{ overflow: "visible" }}
+					style={{ overflow: "visible", position: "relative" }}
 				>
+					<motion.div
+						initial={{ opacity: 0, y: -4, zIndex: 10 }}
+						animate={{ opacity: 1, y: 0, zIndex: 10 }}
+						transition={{ damping: 40 }}
+						style={{ position: "absolute", top: 0, left: 0, right: 0 }}
+					>
+						<Box px="2">
+							<Text
+								color="gray"
+								weight="bold"
+								size="1"
+								style={{
+									letterSpacing: 1,
+									textTransform: "uppercase",
+									background: "var(--color-panel)",
+									padding: "0 0.5rem",
+									position: "absolute",
+									top: "-0.5rem",
+									zIndex: 10,
+								}}
+							>
+								Indoor View
+							</Text>
+						</Box>
+					</motion.div>
 					<Card asChild>
-						<motion.div
-							initial={{ opacity: 0, y: 20 }}
-							animate={{ opacity: 1, y: 0 }}
-							transition={{ damping: 40 }}
-						>
-							<Flex gap="2">
-								<Text color="gray" weight="medium">
-									{closestBuildings
-										?.map((b) => b.properties.name?.en)
-										.join(", ")}
-								</Text>
+						<Box asChild py="3" px="4">
+							<motion.div
+								initial={{ opacity: 0, y: 10, zIndex: 9 }}
+								animate={{ opacity: 1, y: 0, zIndex: 9 }}
+								transition={{ damping: 40 }}
+								style={{ zIndex: 9, position: "relative" }}
+							>
+								<Flex gap="2">
+									<Text weight="medium" size="4" color="plum">
+										{closestBuildings
+											?.map((b) => b.properties.name?.en)
+											.join(", ")}
+									</Text>
 
-								<Text weight="bold">{closestLevel.properties.name?.en}</Text>
-							</Flex>
-						</motion.div>
+									<Text weight="bold" size="4">
+										{closestLevel.properties.name?.en}
+									</Text>
+								</Flex>
+								{/* common things to search */}
+								<Grid columns="4" mt="3">
+									<Flex direction="column" align="center">
+										<IconButton
+											variant="solid"
+											color="plum"
+											size="4"
+											radius="full"
+										>
+											<IconToiletPaper />
+										</IconButton>
+										<Text size="2" mt="2" align="center" weight="medium">
+											Toilets
+										</Text>
+									</Flex>
+									<Flex direction="column" align="center">
+										<IconButton
+											variant="solid"
+											color="cyan"
+											size="4"
+											radius="full"
+										>
+											<IconDroplet />
+										</IconButton>
+										<Text size="2" mt="2" align="center" weight="medium">
+											Water Coolers
+										</Text>
+									</Flex>
+									<Flex direction="column" align="center">
+										<IconButton
+											variant="solid"
+											color="orange"
+											size="4"
+											radius="full"
+										>
+											<IconBus />
+										</IconButton>
+										<Text size="2" mt="2" align="center" weight="medium">
+											ISB Stops
+										</Text>
+									</Flex>
+									<Flex direction="column" align="center">
+										<IconButton
+											variant="solid"
+											color="amber"
+											size="4"
+											radius="full"
+										>
+											<IconBowlChopsticks />
+										</IconButton>
+										<Text size="2" mt="2" align="center" weight="medium">
+											Food & Drinks
+										</Text>
+									</Flex>
+								</Grid>
+							</motion.div>
+						</Box>
 					</Card>
 				</motion.div>
 			)}
